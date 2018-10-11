331d25db7df54897a8d1a4c3d3fb7403630b5086
Damien Hogan <damienhogan87@gmail.com>
2018-10-11 14:07:18 +0100

    Add GA and fix contact and privacy page links

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,67 @@
-const gulp = require("gulp");
-const clean = require("gulp-clean");
-const concat = require("gulp-concat");
-const sass = require("gulp-sass");
-const serve = require("gulp-serve");
-const shell = require("gulp-shell");
-const uglify = require("gulp-uglify");
+const gulp = require('gulp')
+const clean = require('gulp-clean')
+const concat = require('gulp-concat')
+const sass = require('gulp-sass')
+const serve = require('gulp-serve')
+const shell = require('gulp-shell')
+const uglify = require('gulp-uglify-es').default
 
-const { buildSrc, buildDest } = require("./paths");
+const { buildSrc, buildDest } = require('./paths')
 
-gulp.task("setup", function() {
+gulp.task('setup', function() {
   return gulp
-    .src("*.*", {
-      read: false
+    .src('*.*', {
+      read: false,
     })
-    .pipe(gulp.dest(`./${buildDest}`));
-});
+    .pipe(gulp.dest(`./${buildDest}`))
+})
 
-gulp.task("clean", function() {
+gulp.task('clean', function() {
   return gulp
     .src(buildDest, {
-      read: false
+      read: false,
     })
-    .pipe(clean());
-});
+    .pipe(clean())
+})
 
-gulp.task("javascript", function() {
+gulp.task('javascript', function() {
   return gulp
     .src([
-      "node_modules/govuk-frontend/all.js",
-      "node_modules/cookieconsent/build/cookieconsent.min.js",
-      buildSrc + "/assets/js/main.js"
+      'node_modules/govuk-frontend/all.js',
+      'node_modules/cookieconsent/build/cookieconsent.min.js',
+      buildSrc + '/assets/js/main.js',
     ])
     .pipe(uglify())
-    .pipe(concat("script.js"))
-    .pipe(gulp.dest(buildDest + "/assets/js/"));
-});
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest(buildDest + '/assets/js/'))
+})
 
 gulp.task(
-  "serve",
+  'serve',
   serve({
     root: [buildDest],
-    port: 8000
+    port: 8000,
   })
-);
+)
 
-gulp.task("generate", shell.task("eleventy"));
+gulp.task('generate', shell.task('NODE_ENV=development eleventy'))
 
-gulp.task("scss", function() {
+gulp.task('scss', function() {
   return gulp
-    .src(buildSrc + "/assets/scss/main.scss")
+    .src(buildSrc + '/assets/scss/main.scss')
     .pipe(
       sass({
-        outputStyle: "compressed"
-      }).on("error", sass.logError)
+        outputStyle: 'compressed',
+      }).on('error', sass.logError)
     )
-    .pipe(gulp.dest(buildDest + "/assets/css"));
-});
+    .pipe(gulp.dest(buildDest + '/assets/css'))
+})
 
-gulp.task("watch", function() {
-  gulp.watch(buildSrc + "/**/*", gulp.parallel("build"));
-});
+gulp.task('watch', function() {
+  gulp.watch(buildSrc + '/**/*', gulp.parallel('build'))
+})
 
 gulp.task(
-  "build",
-  gulp.series("setup", "clean", "generate", "javascript", "scss")
-);
+  'build',
+  gulp.series('setup', 'clean', 'generate', 'javascript', 'scss')
+)
---
+++ src/_data/env.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+module.exports = {
+  googleAnalyticsApiKey: process.env.GOOGLE_ANALYTICS_KEY,
+}
