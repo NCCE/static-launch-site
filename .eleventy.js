497b749b1f21b51ebc3f41413988c309f14fa932
Damien Hogan <damienh@users.noreply.github.com>
2018-10-11 14:25:49 +0100

    Add GA and fix contact and privacy page links (#20)
    * Add GA and fix contact and privacy page links
    
    * Trigger netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,24 @@
-const { buildSrc, buildDest } = require("./paths");
+const { buildSrc, buildDest } = require('./paths')
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setLibrary("njk", require("nunjucks").configure(""));
-  eleventyConfig.addPassthroughCopy("src/assets/images");
-  eleventyConfig.addPassthroughCopy("src/assets/fonts");
-  eleventyConfig.addCollection("allPages", function(collection) {
-    return collection.getAll();
-  });
+  eleventyConfig.setLibrary('njk', require('nunjucks').configure(''))
+  eleventyConfig.addPassthroughCopy('src/assets/images')
+  eleventyConfig.addPassthroughCopy('src/assets/fonts')
+  eleventyConfig.addCollection('allPages', function(collection) {
+    return collection.getAll()
+  })
+
   return {
-    templateFormats: ["html", "njk"],
-    pathPrefix: "/",
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+    templateFormats: ['html', 'njk'],
+    pathPrefix: '/',
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
     passthroughFileCopy: true,
     dir: {
       input: buildSrc,
       output: buildDest,
-      includes: "_includes"
-    }
-  };
-};
+      includes: '_includes',
+    },
+  }
+}
