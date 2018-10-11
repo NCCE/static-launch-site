0aa3f55df6cf9ac693edcc5c1c2aaac1bccd3bee
Damien Hogan <damienhogan87@gmail.com>
2018-10-11 09:38:41 +0100

    tweak to cookie notice to ensure it uses the correct font

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,10 @@
-window.addEventListener("load", function() {
-  window.cookieconsent.hasTransition = false;
+window.addEventListener('load', function() {
+  window.cookieconsent.hasTransition = false
   window.cookieconsent.initialise({
     showLink: false,
     autoOpen: true,
     overrideHTML:
-      "<div id='global-cookie-message'> <p>We uses cookies to make the site simpler. <a class='govuk-link' href='/privacy-policy'>Learn more</a> <a class='cc-btn cc-dismiss govuk-link' href=''>Ok</a></p></div>",
-    position: "top"
-  });
-});
+      "<div id='global-cookie-message'> <p class='govuk-body'>We uses cookies to make the site simpler. <a class='govuk-link' href='/privacy-policy'>Learn more</a> <a class='cc-btn cc-dismiss govuk-link' href=''>OK</a></p></div>",
+    position: 'top',
+  })
+})
