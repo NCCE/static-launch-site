a974199d6e54d870f964bfdb0faee27b6efdd753
Damien Hogan <damienh@users.noreply.github.com>
2018-10-16 12:08:15 +0100

    Mailing list tweaks (#30)
    * Replace line and you wit higher res png's
    
    * Mailing list section tweaks

--- src/_data/env.js
+++ src/_data/env.js
@@ -2,4 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   googleAnalyticsApiKey: process.env.GOOGLE_ANALYTICS_KEY,
+  mailchimpListID: process.env.MAILCHIMP_LIST_ID,
+  mailchimpUserID: process.env.MAILCHIMP_USER_ID,
 }
