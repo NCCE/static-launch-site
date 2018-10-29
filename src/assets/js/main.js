45be1f6980bf4b942425df25a237eb3e7849422a
Greg Annandale <greg@gregannandale.com>
2018-10-29 11:57:42 +0000

    Privacy page (#50)
    * Privacy page content, markup and styles
    
    * Formatting
    
    * Formatting

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function() {
     showLink: false,
     autoOpen: true,
     overrideHTML:
-      "<div id='global-cookie-message'><div class='cookie-inner'><p class='govuk-body'>We use cookies to make the site simpler. <a class='govuk-link ncce-link__on-light' href='/privacy-policy'>Learn more</a></p> <a class='cc-btn cc-dismiss govuk-link ncce-link__on-light' href=''>OK</a></div></div>",
+      "<div id='global-cookie-message'><div class='cookie-inner'><p class='govuk-body'>We use cookies to make the site simpler. <a class='govuk-link ncce-link__on-light' href='/privacy.html'>Learn more</a></p> <a class='cc-btn cc-dismiss govuk-link ncce-link__on-light' href=''>OK</a></div></div>",
     position: 'top',
   })
 })
